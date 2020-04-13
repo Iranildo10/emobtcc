00733dde0d4fbdd53af5d4989e00f19bf870e7b7
Iranildo10 <48840372+Iranildo10@users.noreply.github.com>
2020-04-13 13:55:39 -0300

    first commit

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -57,11 +57,13 @@ exports.post = async (req, res, next) => {
         );
 
         res.status(201).send({ 
+            erro: 0,
             message: 'Cliente cadastrado com sucesso!'
         });
 
     } catch (e) {
         res.status(400).send({ 
+            erro: 1,
             message: 'Falha ao cadastrar Cliente', 
             data: e.toString()
         });
